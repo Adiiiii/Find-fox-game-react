0ba14b77a11b67a66173ad03244ae636a3b43ac6
Adi <saxenaadi92@gmail.com>
2021-07-21 23:58:21 +0530

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./styles.css";
 import BestGame from "./Pages/BestGame/BestGame";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <BestGame />
+          </Route>
+        </Switch>
+      </Router>
       <BestGame />
     </div>
   );
