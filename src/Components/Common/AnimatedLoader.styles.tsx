fbffa0c8ca2e19bd9c5814b152ff247b99bdff21
Adi <saxenaadi92@gmail.com>
2021-07-25 09:43:52 +0530

    Moved Loader's style to styled components, updated colour

---
+++ src/Components/Common/AnimatedLoader.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Loader from "react-loader-spinner";
+
+export const StyledLoader = styled(Loader)`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
--- src/Components/Common/AnimatedLoader.tsx
+++ src/Components/Common/AnimatedLoader.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Loader from "react-loader-spinner";
+import { StyledLoader } from "./AnimatedLoader.styles";
+import { COLORS } from "../../Constants/Styles";
 
 const AnimatedLoader = () => (
-  <Loader
+  <StyledLoader
     type="Bars"
-    color="#00BFFF"
+    color={COLORS.yellow_dark}
     height={300}
     width={300}
-    style={{
-      position: "fixed",
-      left: "50%",
-      top: "50%",
-      transform: "translate(-50%, -50%)"
-    }}
   />
 );
 
